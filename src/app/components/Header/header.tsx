7cb23b424b1f971406e1a246ae27f3998f7bf815
Rodrigo Abade Nunes <@rodrigo.nunes.est>
2024-10-30 15:35:39 -0300

    First commit

---
+++ src/app/components/Header/header.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import "./header.scss"
+
+export function Header(){
+    return(
+        <div className="header">    
+            <div>
+                <h1>Oi, Eu sou Rodrigo!</h1>
+                <h2>Ciêntista da Computação</h2>
+            </div>
+            <Image
+            src="/me.jpg"
+            alt="Vercel.Logo"
+            width={225}
+            height={210}
+            priority
+            />
+        </div>
+    )
+}
