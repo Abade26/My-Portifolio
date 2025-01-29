e36f7212c77e96a2f45ee13ab589c65d6a1a5983
Rodrigo Abade Nunes <@rodrigo.nunes.est>
2025-01-29 14:04:36 -0300

    Atualizado

---
+++ src/app/components/social-btns/social-btns.tsx
@@ -0,0 +1,24 @@
+import { GitHubIcon } from "../icons/github-icon";
+import { InstaIcon } from "../icons/insta-icon";
+import { LinkedInIcon } from "../icons/linkedin-icon";
+
+import "./social-btns.scss"
+
+interface SocialBtnsProps{
+
+}
+export function SocialBtns(props: SocialBtnsProps){
+    return(
+        <div className="social">
+            <a href="">
+                <InstaIcon/>
+            </a>
+            <a href="">
+                <GitHubIcon/>
+            </a>
+            <a href="">
+                <LinkedInIcon/>
+            </a>
+          </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,9 @@ import { Header } from "./components/Header/header";
 import { Experience } from "./components/experience/experience";
 import "../styles/home.scss";
 import { Info } from "./components/information/information";
+import Image from "next/image";
+import { EmailIcon } from "./components/icons/email-icon";
+import { SocialBtns } from "./components/social-btns/social-btns";
 
 export default function Home() {
   return (
@@ -10,9 +13,11 @@ export default function Home() {
      <Experience/>
       <Info/>
         <div className="buttons">
-          <div className="social">
-          </div>
-          <button className="btn-primary">Contate-me</button>
+          <SocialBtns/>
+          <a className="btn-primary" href="mailto:rodrigoabade26@gmail.com">
+            Contate-me
+            <EmailIcon/>
+          </a>
         </div>
     </main>
   );
